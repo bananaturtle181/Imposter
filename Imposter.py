9f699cf90d7eb38aa3847759add8a4444bdb8f21
bananaturtle181 <lawrenceting35@gmail.com>
2026-05-11 19:36:10 +1000

    Expanded players function by setting min no. players and game mode

--- Imposter.py
+++ Imposter.py
@@ -1,24 +1,39 @@
-def players():
+import random as rd
+
+def players(mode=1):
     players = []
 
     while True:
         try:
             no_players = int(input("How many players are playing?"))
-            confirm = input(f"There are going to be {no_players} playing. Is that correct?(y/n)")
-
-            if confirm in ("y", "Y"):
-                for i in range(1,no_players + 1):
-                    player = {'name': input(f"What is player {i}'s name? ")
-                    }
+            if no_players < 3:
+                print("Minimum 3 players required")
                 break
+            else:
+                confirm = input(f"There are going to be {no_players} playing. Is that correct?(y/n)")
 
-            else: continue
+                if confirm in ("y", "Y"):
+                    for i in range(1,no_players + 1):
+                        player = {'name': input(f"What is player {i}'s name? ")
+                        }
+                        players.append(player)
+                    break
+
+                else: continue
 
         except ValueError:
             print("Please provide a valid whole number")
+            
+    if mode == 1:
 
-    
-    
+
     return
+
+# def word():
+
+
+
+#     return
+
 
 players()
